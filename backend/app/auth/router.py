2d658d80fab121d261db2664027cfafd9d465776
Ryan <rthidden@gmail.com>
2025-07-03 18:32:30 -0500

    fix: Import logging module to enable logging functionality in user authentication

--- backend/app/auth/router.py
+++ backend/app/auth/router.py
@@ -1,5 +1,5 @@
-from datetime import timedelta
 import logging
+from datetime import timedelta
 
 from app.auth.dependencies import get_current_active_user
 from app.auth.utils import (create_access_token, get_password_hash,
